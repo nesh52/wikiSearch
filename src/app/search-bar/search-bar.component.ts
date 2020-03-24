c497c0b2ee5d98f94cbc960bee29073330a710cb
nesh52 <nesh86ster@gmail.com>
2020-03-24 22:39:06 +0400

    added service

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  term = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    console.log(this.term);
+
+  }
 }
