a3b340029640e731c5ae8d681522e19dc58fdb5e
nesh52 <nesh86ster@gmail.com>
2020-03-25 16:19:09 +0400

    parent to child communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,13 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
 
-  constructor(private wikipedia: WikipediaService) {
+  pages = [];
 
-  }
+  constructor(private wikipedia: WikipediaService) {  }
   
   onTerm(term: string) {
-   this.wikipedia.search(term).subscribe((response) => {
-      console.log(response);
+   this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
    });
   }
 }
