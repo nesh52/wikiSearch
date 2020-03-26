df8013c928a972fc30ae3e982fcb658cb004b1d4
nesh52 <nesh86ster@gmail.com>
2020-03-26 13:32:01 +0400

    improved by adding observable and interface

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string,
+      pageid: number,
+      snippet: string
+    }[]
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +20,7 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   public search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
         format: 'json',
@@ -18,6 +29,7 @@ export class WikipediaService {
         srsearch: term,
         origin: '*'
       }
-    });
+    })
+    .pipe(pluck('query', 'search'));
   }
 }
